d2c0292f7ccc88bb59424e86b07c3f40c3211346
Unknown <saszaaleksandrowicz@gmail.com>
2019-06-27 00:26:15 +0200

    Lesson #2.0

--- src/main/kotlin/pl/womentocode/ticket/TicketApplication.kt
+++ src/main/kotlin/pl/womentocode/ticket/TicketApplication.kt
@@ -1,5 +1,6 @@
 package pl.womentocode.ticket
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
--- src/main/kotlin/pl/womentocode/ticket/web/GreetingController.kt
+++ src/main/kotlin/pl/womentocode/ticket/web/GreetingController.kt
@@ -1,12 +1,24 @@
 package pl.womentocode.ticket.web
 
+import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Profile("default")
 @RequestMapping("/greet")
 class GreetingController {
+
+    @RequestMapping("","/{person}")
+    fun greetPerson(@PathVariable(name = "person", required = false) person : String?)=
+            "Hello ${person?:"PlayerUnknown"}"
+}
+
+@Profile("dev")
+@RestController
+@RequestMapping("/devgreet")
+class GreetingDevController {
 
     @RequestMapping("","/{person}")
     fun greetPerson(@PathVariable(name = "person", required = false) person : String?)=
